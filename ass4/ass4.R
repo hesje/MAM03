cfa2d242059b640a7a996da283a827f0ac8cbbf6
Hessel Bierma <h.bierma@me.com>
2021-11-23 19:04:24 +0100

    Update ass4.R

--- ass4/ass4.R
+++ ass4/ass4.R
@@ -2,24 +2,80 @@ library(nlme)
 library(survival)
 library(JM)
 library(tidyverse)
+library(ggplot2)
+library(survminer)
 
+############################################## Data
 load("kidney transplant.RData")
 
-d$years = d$time_to_death
-dlongg = dlong %>% drop_na(gfr)
+d$sexDonEqual = d$sexdon == d$sex_pat
+dlong$sexDonEqual = dlong$sexdon == dlong$sex_pat
 
-l = lme(gfr ~ sex_pat + age_at_tx + bmi + retrans + years + sexdon, random = ~1 + years|ID, data = dlongg, na.action = na.exclude)
+dcox = dlong
+dcox$start = dcox$years
+dcox$end[dcox$years == 0.25] = 0.5
+dcox$end[dcox$years == 0.5] = 1
+dcox$end[dcox$years == 1] = 2
+dcox$end[dcox$years == 2] = 5
+dcox$end[dcox$years == 5] = 10
+dcox$end[dcox$years == 10] = 15
+dcox$end[dcox$years == 15] = 20
 
-preds = predict(l, level = 1, data = dlongg)
 
-plot(dlongg$years[dlongg$ID==2], dlongg$gfr[dlongg$ID==2])
-lines(dlongg$years[dlongg$ID==2], preds[dlongg$ID==2])
 
-cox = coxph(Surv(d$time_to_death, d$stat_pat) ~ d$sex_pat + d$bmi + d$gfr1 + d$screat1 + d$map1 + cluster(d$ID), data=d, x=TRUE, id=d$ID, model = TRUE, na.action = na.pass)
+for (i in unique(dcox$ID)) {
+  dcox$end[dcox$ID == i & dcox$start < dcox$time_to_death & dcox$end > dcox$time_to_death] = dcox$time_to_death[dcox$ID == i][1]
+}
 
-years = dlongg$years
+for (j in 1:dim(dcox)[1]) {
+  dd = dcox[j,]
+  dcox$gEvent[j] = dd$time_to_graft_failure > dd$start & dd$time_to_graft_failure <= dd$end & dd$stat_gra == "Graft loss"
+  dcox$dEvent[j] = dd$time_to_death > dd$start & dd$time_to_death <= dd$end & dd$stat_pat == "overleden"
+}
 
-j = jointModel(l, cox, timeVar="years")
+dcox = subset(dcox, start < time_to_death)
+
+gcox = subset(dcox, start < time_to_graft_failure)
+
+for (i in unique(gcox$ID)) {
+  gcox$end[gcox$ID == i & gcox$start < gcox$time_to_graft_failure & gcox$end > gcox$time_to_graft_failure] = gcox$time_to_graft_failure[gcox$ID == i][1]
+}
+
+######################################################## models
+## COX Baseline models
+cbld = coxph(Surv(time_to_death, stat_pat) ~ sex_pat + sexDonEqual + bmi + age_at_tx + type_dia + retrans + duur_dia, data=d, x=TRUE, model = TRUE, na.action = na.exclude, id = d$ID)
+cblg = coxph(Surv(time_to_graft_failure, stat_gra) ~ sex_pat + sexDonEqual + bmi + age_at_tx + type_dia + retrans + duur_dia, data=d, x=TRUE, model = TRUE, na.action = na.exclude, id = d$ID)
+
+ggforest(cbld, data = d)
+
+## LME
+l = lme(gfr ~ sex_pat + age_at_tx + bmi + retrans + years + sexdon + type_dia , random = ~1 + years|ID, data = dcox, na.action = na.exclude)
+
+preds = predict(l, level = 1, data = dcox)
+
+plotPredLme = function(pt){
+  plot(dcox$years[dcox$ID==pt], dcox$gfr[dcox$ID==pt])
+  lines(dcox$years[dcox$ID==pt], preds[dcox$ID==pt])
+}
+
+## COX for time to death
+cd = coxph(Surv(start, end, dEvent) ~ sex_pat + bmi + age_at_tx + type_dia + sexDonEqual + preds + cluster(dcox$ID), data=dcox, x=TRUE, model = TRUE, na.action = na.exclude)
+ggforest(cd, data = dcox)
+
+## COX for time to graft failure
+cg = coxph(Surv(start, end, gEvent) ~ sex_pat + bmi + gfr + creat + map + age_at_tx + type_dia + sexdon + cluster(gcox$ID), data=gcox, x=TRUE, id=gcox$ID, model = TRUE, na.action = na.exclude)
+ggforest(cg, data = gcox)
+
+mod = survfit(Surv(start, end, dEvent) ~ type_dia, data = dcox)
+plot(mod)
+
+plot(cox.zph(cd))
+cox.zph(cd)
+
+## Joint model
+years = dcox$years
+
+j = jointModel(l, cd, timeVar="years")
 
 predss = predict(j, level = 1, newdata = dlongg)
 
